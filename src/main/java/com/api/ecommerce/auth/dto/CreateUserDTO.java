9b8ff1b41780feab8ab46563baf1c20d8ff8360c
Mohamed Hany <mohamed.hany556@gmail.com>
2024-06-19 16:23:08 +0300

    fix validation error

--- src/main/java/com/api/ecommerce/auth/dto/CreateUserDTO.java
+++ src/main/java/com/api/ecommerce/auth/dto/CreateUserDTO.java
@@ -1,35 +1,29 @@
 package com.api.ecommerce.auth.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public class CreateUserDTO {
 
     @NotNull(message = "{user.email.notblank}")
     @NotBlank(message = "{user.email.notblank}")
-    @Max(value = 255,message = "{user.email.size}")
+    @Email(message = "{user.email.valid}")
+    @Size(max = 255, message = "{user.email.size}")
     private String email;
 
     @NotNull(message = "{user.password.notblank}")
     @NotBlank(message = "{user.password.notblank}")
-    @Min(value = 6,message = "{user.password.size}")
-    @Max(value = 255,message = "{user.password.size}")
+    @Size(min = 6, max = 255, message = "{user.password.size}")
     private String password;
 
     @NotNull(message = "{user.first_name.notblank}")
     @NotBlank(message = "{user.first_name.notblank}")
-    @Min(value = 2,message = "{user.first_name.size}")
-    @Max(value = 255,message = "{user.first_name.size}")
+    @Size(min = 2, max = 255, message = "{user.first_name.size}")
     private String firstName;
 
     @NotNull(message = "{user.last_name.notblank}")
     @NotBlank(message = "{user.last_name.notblank}")
-    @Min(value = 2,message = "{user.last_name.size}")
-    @Max(value = 255,message = "{user.last_name.size}")
+    @Size(min = 2, max = 255, message = "{user.last_name.size}")
     private String lastName;
-
 
     public String getEmail() {
         return email;
